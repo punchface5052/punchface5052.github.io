f8c7c999dc8fb3b0cb6ee9b069d55e87580fa98f
Punch <146510888+PunchFace505@users.noreply.github.com>
2024-09-25 14:17:13 -0600

    WOO BIG PROGRESS ON INTERACTIVE PROJECT!

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -7,18 +7,29 @@
 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
 }
 let arrowX;
 let arrowY;
-let 
-let mouseD = 50;
+let leftAngle;
+let mouseD = 10;
 let charX = 50;
 let charY = 50;
 let charD = 100;
+let attackWidth = 50;
+let attackHeight = 25;
+let attackDistance = 1.25;
 let speed = 5;
-let time = 0;
+let weaponState = "weapon1";
+let attacking = false;
+let lastAttack = 0;
+let attackSpeed = 1;
+const ATTACK_DUR = 1000;
+const WEAPON_1_WAIT = 1000;
+const WEAPON_2_WAIT = 2000;
+const WEAPON_3_WAIT = 3000;
+
 
 function moveChar() {
   if (keyIsDown(87)) { //w
@@ -35,36 +46,51 @@ function moveChar() {
   }
 }
 
+function attack(){
+  if (weaponState === "weapon1" && mouseIsPressed && millis() > lastAttack + WEAPON_1_WAIT){
+    attackSpeed = 4;
+    attacking = true;
+    lastAttack = millis();
+  }
+  else if (weaponState === "weapon2" &&mouseIsPressed && millis() > lastAttack + WEAPON_2_WAIT){
+    attackSpeed = 2;
+    attacking = true;
+    lastAttack = millis();
+  }
+  else if (weaponState === "weapon3" &&mouseIsPressed && millis() > lastAttack + WEAPON_3_WAIT){
+    attackSpeed = 1;
+    attacking = true;
+    lastAttack = millis();
+  }
+  else if (millis() > lastAttack + ATTACK_DUR/attackSpeed){
+    attacking = false;
+  }
+}
+
 function charDisp(){
-  arrowX = charX+1/2*d;
+  arrowX = charX+1/2*charD;
   arrowY = charY;
   leftAngle = atan2(mouseY - arrowY, mouseX - arrowX);
   translate(arrowX, arrowY);
   fill(255);
   circle(0, 0, charD);
   rotate(leftAngle);
-  fill(0);
-  ellipse(d*1.25, 0, 25, 25);
-}
-
-function mouseDisp(){
-  circle(mouseX,mouseY,mouseD)
-}
-
-// function attack(){
-//   stroke(0)
-//   if (mousePressed&&time===0){
-//     line(charX,charY,mouseX,mouseY)
-
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (attacking){
+    fill(0);
+    ellipse(charD*attackDistance, 0, attackWidth, attackHeight);
+    fill(255);
+  }
+  else{
+    fill(255);
+    ellipse(charD*attackDistance, 0, attackWidth, attackHeight);
+  }
 }
 
 function draw() {
   background(255);
   moveChar();
-  charDisp();
-  mouseDisp();
+  attack();
+  fill(0);
   text("Cheese",windowWidth/2,windowHeight/2);
+  charDisp();
 }
