7d3bffdd87a759871ba642998dae88220143e9c5
Punch <liam.thorpe505@gmail.com>
2024-11-12 14:16:06 -0600

    I forgot I needed two different sprites for black and white sides

--- grid-project/sketch.js
+++ grid-project/sketch.js
@@ -6,26 +6,42 @@
 // - describe what you did to take this project "above and beyond"
 
 let state = "title";
-let grid;
+let grid, cellSize;
 const GRID_SIZE = 8;
 const BLACK = 1;
 const WHITE = 0;
-let cellSize;
 let side = "white";
-let babybel;
+let babybel, wKing, wQueen, wRook, wKnight, wBishop, wPawn, bKing, bQueen, bRook, bKnight, bBishop, bPawn, spugnebob;
 let pieceClicked = false;
 
-
+let startGrid = [
+  ['r','n','b','q','k','b','n','r'],
+  ['p','p','p','p','p','p','p','p'],
+  [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+  [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+  [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+  [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+  ['p','p','p','p','p','p','p','p'],
+  ['r','n','b','q','k','b','n','r']
+];
 
 function preload(){
   babybel = loadImage("cheeses/babybel.png");
-  loadImage("cheeses/cheeseking.png");
-  loadImage("cheeses/cheesemoon.png");
-  loadImage("cheeses/cheesepawn.png");
-  loadImage("cheeses/defnotspugneboobcheese.png");
-  loadImage("cheeses/pizzacheese.png");
-  loadImage("cheeses/skyrimcheesewheel.png");
-  loadImage("cheeses/swisscheese.png");
+  spugnebob = loadImage("cheeses/defnotspugneboobcheese.png");
+
+  wKing = loadImage("cheeses/wcheeseking.png");
+  wRook = loadImage("cheeses/wcheesemoon.png");
+  wPawn = loadImage("cheeses/wcheesepawn.png");
+  wQueen = loadImage("cheeses/wpizzacheese.png");
+  wKnight = loadImage("cheeses/wskyrimcheesewheel.png");
+  wBishop = loadImage("cheeses/wswisscheese.png");
+
+  bQueen = loadImage("cheeses/bpizzacheese.png");
+  bKnight = loadImage("cheeses/bskyrimcheesewheel.png");
+  bBishop = loadImage("cheeses/bswisscheese.png");
+  bKing = loadImage("cheeses/bcheeseking.png");
+  bRook = loadImage("cheeses/bcheesemoon.png");
+  bPawn = loadImage("cheeses/bcheesepawn.png");
   
 }
 
